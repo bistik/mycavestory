5be1539fd223a62d532d67ca38bc34073f6aa108
bistik <bistikred@gmail.com>
2017-10-16 13:03:50 +0800

    episode 2 and 3

--- source/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <SDL2/SDL.h>
-
-int main(int argc, const char* argv[])
-{
-	printf("Hi, hello\n");
-	return 0;
-}
---
+++ source/src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * main.cpp
+ * entry point of the program
+ */
+#include "game.h"
+
+int main(int argc, const char* argv[])
+{
+	Game game;
+}
